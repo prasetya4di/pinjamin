8313532f47cc8a0a80b338c7fe2e086830a72fb7
praset <prasetyaarfanto@gmail.com>
2022-07-07 15:48:19 +0700

    add loaning dao

---
+++ app/src/main/java/com/project/pinjamin/data/local/admin/LoaningDao.kt
@@ -0,0 +1,28 @@
+package com.project.pinjamin.data.local.admin
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.project.pinjamin.model.entity.Loaning
+import com.project.pinjamin.model.relation.LoaningWithDetails
+
+@Dao
+interface LoaningDao {
+    @Insert
+    fun insert(loaningDao: Dao)
+
+    @Query("Select * from peminjaman")
+    fun get(): LiveData<List<Loaning>>
+
+    @Query("Select * from peminjaman where id_peminjaman = :id")
+    fun get(id: Int): LiveData<Loaning>
+
+    @Transaction
+    @Query("Select * from peminjaman where id_peminjaman = :id")
+    fun getLoaningWithDetails(id: Int): LiveData<LoaningWithDetails>
+
+    @Update
+    fun update(loaning: Loaning)
+
+    @Delete
+    fun delete(loaning: Loaning)
+}
