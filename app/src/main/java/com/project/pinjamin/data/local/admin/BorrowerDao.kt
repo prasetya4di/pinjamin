2ed656bcdfa81c0c489b7e869c7035f8eec467b5
praset <prasetyaarfanto@gmail.com>
2022-07-07 15:37:06 +0700

    add borrower dao

---
+++ app/src/main/java/com/project/pinjamin/data/local/admin/BorrowerDao.kt
@@ -0,0 +1,23 @@
+package com.project.pinjamin.data.local.admin
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.project.pinjamin.model.entity.Borrower
+
+@Dao
+interface BorrowerDao {
+    @Insert
+    fun insert(borrower: Borrower)
+
+    @Query("Select * from peminjam")
+    fun get(): LiveData<List<Borrower>>
+
+    @Query("Select * from peminjam where id_peminjam = :id")
+    fun get(id: Int): LiveData<Borrower>
+
+    @Update
+    fun update(borrower: Borrower)
+
+    @Delete
+    fun delete(borrower: Borrower)
+}
