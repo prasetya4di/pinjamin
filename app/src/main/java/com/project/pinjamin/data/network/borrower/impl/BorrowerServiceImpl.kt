078056d2b27d0e04133081443881f0f6ecfdf266
praset <prasetyaarfanto@gmail.com>
2022-07-07 23:10:44 +0700

    add borrower service implementation

---
+++ app/src/main/java/com/project/pinjamin/data/network/borrower/impl/BorrowerServiceImpl.kt
@@ -0,0 +1,23 @@
+package com.project.pinjamin.data.network.borrower.impl
+
+import com.project.pinjamin.data.network.borrower.BorrowerService
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.model.entity.Borrower
+
+class BorrowerServiceImpl : BorrowerService {
+    override fun insert(borrower: Borrower): Status {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(borrower: Borrower): Status {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(borrower: Borrower): Status {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(): List<Borrower> {
+        TODO("Not yet implemented")
+    }
+}
