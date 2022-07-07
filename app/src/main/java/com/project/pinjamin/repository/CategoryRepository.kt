9e967e54a4870b815c6ffa675b17667ff32e4794
praset <prasetyaarfanto@gmail.com>
2022-07-07 22:02:02 +0700

    add necessary function for category repository

--- app/src/main/java/com/project/pinjamin/repository/CategoryRepository.kt
+++ app/src/main/java/com/project/pinjamin/repository/CategoryRepository.kt
@@ -1,3 +1,16 @@
 package com.project.pinjamin.repository
 
-interface CategoryRepository
+import androidx.lifecycle.LiveData
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.model.entity.Category
+import com.project.pinjamin.model.relation.CategoryWithItems
+import kotlinx.coroutines.flow.Flow
+
+interface CategoryRepository {
+    fun insert(category: Category): Flow<Status>
+    fun update(category: Category): Flow<Status>
+    fun delete(category: Category): Flow<Status>
+    fun get(): LiveData<List<Category>>
+    fun get(id: Int): LiveData<Category>
+    fun getCategoryWithItems(id: Int): LiveData<CategoryWithItems>
+}
