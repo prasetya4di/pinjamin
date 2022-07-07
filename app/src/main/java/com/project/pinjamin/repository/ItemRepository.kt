540496a02300c5b24e731589877c22660d6d28e1
praset <prasetyaarfanto@gmail.com>
2022-07-07 22:07:27 +0700

    add necessary function for item repository

--- app/src/main/java/com/project/pinjamin/repository/ItemRepository.kt
+++ app/src/main/java/com/project/pinjamin/repository/ItemRepository.kt
@@ -1,3 +1,14 @@
 package com.project.pinjamin.repository
 
-interface ItemRepository
+import androidx.lifecycle.LiveData
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.model.entity.Item
+import kotlinx.coroutines.flow.Flow
+
+interface ItemRepository {
+    fun insert(item: Item): Flow<Status>
+    fun update(item: Item): Flow<Status>
+    fun delete(item: Item): Flow<Status>
+    fun get(): LiveData<List<Item>>
+    fun get(id: Int): LiveData<Item>
+}
