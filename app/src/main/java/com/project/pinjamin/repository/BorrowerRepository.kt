9ca7a574a383046fb7e9f9ea3ba4a05f933ce03f
praset <prasetyaarfanto@gmail.com>
2022-07-07 21:58:51 +0700

    add necessary function for borrower repository

--- app/src/main/java/com/project/pinjamin/repository/BorrowerRepository.kt
+++ app/src/main/java/com/project/pinjamin/repository/BorrowerRepository.kt
@@ -1,3 +1,14 @@
 package com.project.pinjamin.repository
 
-interface BorrowerRepository
+import androidx.lifecycle.LiveData
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.model.entity.Borrower
+import kotlinx.coroutines.flow.Flow
+
+interface BorrowerRepository {
+    fun insert(borrower: Borrower): Flow<Status>
+    fun update(borrower: Borrower): Flow<Status>
+    fun delete(borrower: Borrower): Flow<Status>
+    fun get(): LiveData<List<Borrower>>
+    fun get(id: Int): LiveData<Borrower>
+}
