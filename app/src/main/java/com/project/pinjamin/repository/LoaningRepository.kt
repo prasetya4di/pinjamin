1eefcaeb3bf8aede6fac0d59285c924723cf3887
praset <prasetyaarfanto@gmail.com>
2022-07-07 22:08:21 +0700

    add necessary function for loaning repository

--- app/src/main/java/com/project/pinjamin/repository/LoaningRepository.kt
+++ app/src/main/java/com/project/pinjamin/repository/LoaningRepository.kt
@@ -1,3 +1,16 @@
 package com.project.pinjamin.repository
 
-interface LoaningRepository
+import androidx.lifecycle.LiveData
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.model.entity.Loaning
+import com.project.pinjamin.model.relation.LoaningWithDetails
+import kotlinx.coroutines.flow.Flow
+
+interface LoaningRepository {
+    fun insert(loaning: Loaning): Flow<Status>
+    fun update(loaning: Loaning): Flow<Status>
+    fun delete(loaning: Loaning): Flow<Status>
+    fun get(): LiveData<List<Loaning>>
+    fun get(id: Int): LiveData<Loaning>
+    fun getLoaningWithDetails(): LiveData<List<LoaningWithDetails>>
+}
