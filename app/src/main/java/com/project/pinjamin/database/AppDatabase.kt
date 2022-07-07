f1f6da387c21063579f3ec13e81ac993fa258c99
praset <prasetyaarfanto@gmail.com>
2022-07-07 16:14:13 +0700

    use dao in app database

--- app/src/main/java/com/project/pinjamin/database/AppDatabase.kt
+++ app/src/main/java/com/project/pinjamin/database/AppDatabase.kt
@@ -3,6 +3,12 @@ package com.project.pinjamin.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.project.pinjamin.data.local.admin.AdminDao
+import com.project.pinjamin.data.local.borrower.BorrowerDao
+import com.project.pinjamin.data.local.category.CategoryDao
+import com.project.pinjamin.data.local.item.ItemDao
+import com.project.pinjamin.data.local.loaning.LoaningDao
+import com.project.pinjamin.data.local.loaning_detail.LoaningDetailDao
 import com.project.pinjamin.model.entity.*
 import com.project.pinjamin.model.reference.LoaningDetailItemCrossRef
 import com.project.pinjamin.util.Converters
@@ -12,4 +18,11 @@ import com.project.pinjamin.util.Converters
     version = 1
 )
 @TypeConverters(Converters::class)
-abstract class AppDatabase : RoomDatabase()
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun adminDao(): AdminDao
+    abstract fun borrowerDao(): BorrowerDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun itemDao(): ItemDao
+    abstract fun loaningDao(): LoaningDao
+    abstract fun loaningDetailDao(): LoaningDetailDao
+}
