01f5767d726e33003caa41c12216990aadeb7698
praset <prasetyaarfanto@gmail.com>
2022-07-07 22:53:22 +0700

    change database name

--- app/src/main/java/com/project/pinjamin/di/DatabaseModule.kt
+++ app/src/main/java/com/project/pinjamin/di/DatabaseModule.kt
@@ -55,7 +55,7 @@ class DatabaseModule {
         return Room.databaseBuilder(
             appContext,
             AppDatabase::class.java,
-            "BankSampah"
+            "Pinjamin"
         ).build()
     }
 }
