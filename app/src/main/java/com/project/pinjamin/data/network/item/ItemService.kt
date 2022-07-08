bd4b380ce2c5da6a25a54fea476a27a07606545d
praset <prasetyaarfanto@gmail.com>
2022-07-08 13:46:43 +0700

    add item service implementation

--- app/src/main/java/com/project/pinjamin/data/network/item/ItemService.kt
+++ app/src/main/java/com/project/pinjamin/data/network/item/ItemService.kt
@@ -1,11 +1,10 @@
 package com.project.pinjamin.data.network.item
 
-import com.project.pinjamin.enum.Status
 import com.project.pinjamin.model.entity.Item
 
 interface ItemService {
-    fun insert(item: Item): Status
-    fun update(item: Item): Status
-    fun delete(item: Item): Status
+    fun insert(item: Item)
+    fun update(item: Item)
+    fun delete(item: Item)
     fun get(): List<Item>
 }
