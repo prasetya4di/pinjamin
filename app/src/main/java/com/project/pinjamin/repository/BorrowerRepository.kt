a9757aa014a979d019d0cfbcdbb58e5e799b6d35
praset <prasetyaarfanto@gmail.com>
2022-07-08 10:12:10 +0700

    update borrower repository to also use fetch data

--- app/src/main/java/com/project/pinjamin/repository/BorrowerRepository.kt
+++ app/src/main/java/com/project/pinjamin/repository/BorrowerRepository.kt
@@ -1,15 +1,17 @@
 package com.project.pinjamin.repository
 
 import androidx.lifecycle.LiveData
-import com.project.pinjamin.enum.Status
 import com.project.pinjamin.model.entity.Borrower
-import kotlinx.coroutines.flow.Flow
 
 interface BorrowerRepository {
-    fun insert(borrower: Borrower): Flow<Status>
-    fun insert(borrowers: List<Borrower>): Flow<Status>
-    fun update(borrower: Borrower): Flow<Status>
-    fun delete(borrower: Borrower): Flow<Status>
+    fun insert(borrower: Borrower)
+    fun insert(borrowers: List<Borrower>)
+    fun fetchInsert(borrower: Borrower)
+    fun update(borrower: Borrower)
+    fun fetchUpdate(borrower: Borrower)
+    fun delete(borrower: Borrower)
+    fun fetchDelete(borrower: Borrower)
     fun get(): LiveData<List<Borrower>>
     fun get(id: Int): LiveData<Borrower>
+    fun fetchGet(): List<Borrower>
 }
