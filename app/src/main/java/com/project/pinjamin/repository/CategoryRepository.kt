9711983763e28a8737764f8b803588b36d0b6c84
praset <prasetyaarfanto@gmail.com>
2022-07-08 13:38:19 +0700

    add fetch function in category repository

--- app/src/main/java/com/project/pinjamin/repository/CategoryRepository.kt
+++ app/src/main/java/com/project/pinjamin/repository/CategoryRepository.kt
@@ -1,17 +1,19 @@
 package com.project.pinjamin.repository
 
 import androidx.lifecycle.LiveData
-import com.project.pinjamin.enum.Status
 import com.project.pinjamin.model.entity.Category
 import com.project.pinjamin.model.relation.CategoryWithItems
-import kotlinx.coroutines.flow.Flow
 
 interface CategoryRepository {
-    fun insert(category: Category): Flow<Status>
-    fun insert(categories: List<Category>): Flow<Status>
-    fun update(category: Category): Flow<Status>
-    fun delete(category: Category): Flow<Status>
+    fun insert(category: Category)
+    fun insert(categories: List<Category>)
+    fun fetchInsert(category: Category)
+    fun update(category: Category)
+    fun fetchUpdate(category: Category)
+    fun delete(category: Category)
+    fun fetchDelete(category: Category)
     fun get(): LiveData<List<Category>>
     fun get(id: Int): LiveData<Category>
     fun getCategoryWithItems(id: Int): LiveData<CategoryWithItems>
+    fun fetchCategory(): List<Category>
 }
