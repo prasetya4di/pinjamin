9d8f853e8b54b5d6263539424f739bdbd6cf355a
praset <prasetyaarfanto@gmail.com>
2022-07-08 13:40:32 +0700

    add category service implementation

--- app/src/main/java/com/project/pinjamin/data/network/category/CategoryService.kt
+++ app/src/main/java/com/project/pinjamin/data/network/category/CategoryService.kt
@@ -1,11 +1,10 @@
 package com.project.pinjamin.data.network.category
 
-import com.project.pinjamin.enum.Status
 import com.project.pinjamin.model.entity.Category
 
 interface CategoryService {
-    fun insert(category: Category): Status
-    fun update(category: Category): Status
-    fun delete(category: Category): Status
+    fun insert(category: Category)
+    fun update(category: Category)
+    fun delete(category: Category)
     fun get(): List<Category>
 }
---
+++ app/src/main/java/com/project/pinjamin/data/network/category/impl/CategoryServiceImpl.kt
@@ -0,0 +1,22 @@
+package com.project.pinjamin.data.network.category.impl
+
+import com.project.pinjamin.data.network.category.CategoryService
+import com.project.pinjamin.model.entity.Category
+
+class CategoryServiceImpl : CategoryService {
+    override fun insert(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(): List<Category> {
+        TODO("Not yet implemented")
+    }
+}
