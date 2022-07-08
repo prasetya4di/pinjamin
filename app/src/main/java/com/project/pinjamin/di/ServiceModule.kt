61147885f5652cf4b2abc7c0af25433e462759c9
praset <prasetyaarfanto@gmail.com>
2022-07-08 14:44:40 +0700

    update service module

--- app/src/main/java/com/project/pinjamin/di/ServiceModule.kt
+++ app/src/main/java/com/project/pinjamin/di/ServiceModule.kt
@@ -2,6 +2,16 @@ package com.project.pinjamin.di
 
 import com.project.pinjamin.data.network.admin.AdminService
 import com.project.pinjamin.data.network.admin.impl.AdminServiceImpl
+import com.project.pinjamin.data.network.borrower.BorrowerService
+import com.project.pinjamin.data.network.borrower.impl.BorrowerServiceImpl
+import com.project.pinjamin.data.network.category.CategoryService
+import com.project.pinjamin.data.network.category.impl.CategoryServiceImpl
+import com.project.pinjamin.data.network.item.ItemService
+import com.project.pinjamin.data.network.item.impl.ItemServiceImpl
+import com.project.pinjamin.data.network.loaning.LoaningService
+import com.project.pinjamin.data.network.loaning.impl.LoaningServiceImpl
+import com.project.pinjamin.data.network.loaning_detail.LoaningDetailService
+import com.project.pinjamin.data.network.loaning_detail.impl.LoaningDetailServiceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,4 +24,24 @@ class ServiceModule {
     @Provides
     @Singleton
     fun provideAdminService(): AdminService = AdminServiceImpl()
+
+    @Provides
+    @Singleton
+    fun provideBorrowerService(): BorrowerService = BorrowerServiceImpl()
+
+    @Provides
+    @Singleton
+    fun provideCategoryService(): CategoryService = CategoryServiceImpl()
+
+    @Provides
+    @Singleton
+    fun provideItemService(): ItemService = ItemServiceImpl()
+
+    @Provides
+    @Singleton
+    fun provideLoaningService(): LoaningService = LoaningServiceImpl()
+
+    @Provides
+    @Singleton
+    fun provideLoaningDetailService(): LoaningDetailService = LoaningDetailServiceImpl()
 }
