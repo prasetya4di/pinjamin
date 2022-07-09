aea5c5696000424c855acbc8cb5254ee1c079c32
praset <prasetyaarfanto@gmail.com>
2022-07-09 10:15:02 +0700

    add delete borrower use case

---
+++ app/src/main/java/com/project/pinjamin/usecase/DeleteBorrowerUseCase.kt
@@ -0,0 +1,9 @@
+package com.project.pinjamin.usecase
+
+import com.project.pinjamin.data.database.entity.Borrower
+import com.project.pinjamin.enum.Status
+import kotlinx.coroutines.flow.Flow
+
+interface DeleteBorrowerUseCase {
+    operator fun invoke(borrower: Borrower): Flow<Status>
+}
---
+++ app/src/main/java/com/project/pinjamin/usecase/impl/DeleteBorrowerUseCaseImpl.kt
@@ -0,0 +1,20 @@
+package com.project.pinjamin.usecase.impl
+
+import com.project.pinjamin.data.database.entity.Borrower
+import com.project.pinjamin.data.repository.BorrowerRepository
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.usecase.DeleteBorrowerUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class DeleteBorrowerUseCaseImpl @Inject constructor(
+    private val borrowerRepository: BorrowerRepository
+) : DeleteBorrowerUseCase {
+    override fun invoke(borrower: Borrower): Flow<Status> = flow {
+        emit(Status.LOADING)
+        borrowerRepository.delete(borrower)
+        borrowerRepository.fetchDelete(borrower)
+        emit(Status.SUCCESS)
+    }
+}
