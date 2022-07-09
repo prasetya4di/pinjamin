cc6f2a927911c96e7f78dca3e2956c075e94a11f
praset <prasetyaarfanto@gmail.com>
2022-07-09 15:14:06 +0700

    add delete item use case

---
+++ app/src/main/java/com/project/pinjamin/usecase/item/impl/DeleteItemUseCaseImpl.kt
@@ -0,0 +1,20 @@
+package com.project.pinjamin.usecase.item.impl
+
+import com.project.pinjamin.data.database.entity.Item
+import com.project.pinjamin.data.repository.ItemRepository
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.usecase.item.DeleteItemUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class DeleteItemUseCaseImpl @Inject constructor(
+    private val itemRepository: ItemRepository
+) : DeleteItemUseCase {
+    override fun invoke(item: Item): Flow<Status> = flow {
+        emit(Status.LOADING)
+        itemRepository.fetchDelete(item)
+        itemRepository.delete(item)
+        emit(Status.SUCCESS)
+    }
+}
