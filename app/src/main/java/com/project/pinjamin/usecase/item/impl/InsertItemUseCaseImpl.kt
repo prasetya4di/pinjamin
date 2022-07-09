2972f9e42fe49934d7d019945dad9e410350b28f
praset <prasetyaarfanto@gmail.com>
2022-07-09 15:11:01 +0700

    add insert item use case

---
+++ app/src/main/java/com/project/pinjamin/usecase/item/impl/InsertItemUseCaseImpl.kt
@@ -0,0 +1,20 @@
+package com.project.pinjamin.usecase.item.impl
+
+import com.project.pinjamin.data.database.entity.Item
+import com.project.pinjamin.data.repository.ItemRepository
+import com.project.pinjamin.enum.Status
+import com.project.pinjamin.usecase.item.InsertItemUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class InsertItemUseCaseImpl @Inject constructor(
+    private val itemRepository: ItemRepository
+) : InsertItemUseCase {
+    override fun invoke(item: Item): Flow<Status> = flow {
+        emit(Status.LOADING)
+        itemRepository.fetchInsert(item)
+        itemRepository.insert(item)
+        emit(Status.SUCCESS)
+    }
+}
