3e6a34ba15a252050d8abc1b6655434b2d722ee5
praset <prasetyaarfanto@gmail.com>
2022-07-12 07:25:24 +0700

    add activity login

---
+++ app/src/main/java/com/project/pinjamin/ui/login/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.project.pinjamin.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project.pinjamin.MainActivity
+import com.project.pinjamin.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+        }
+    }
+}
