d46bcaf3dcc51ca15544ea2fe606c4c88622c26c
praset <prasetyaarfanto@gmail.com>
2022-07-12 07:47:58 +0700

    add loaner fragment

---
+++ app/src/main/java/com/project/pinjamin/ui/loaner/LoanerFragment.kt
@@ -0,0 +1,32 @@
+package com.project.pinjamin.ui.loaner
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.project.pinjamin.R
+
+class LoanerFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoanerFragment()
+    }
+
+    private lateinit var viewModel: LoanerViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loaner, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoanerViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/project/pinjamin/ui/loaner/LoanerViewModel.kt
@@ -0,0 +1,7 @@
+package com.project.pinjamin.ui.loaner
+
+import androidx.lifecycle.ViewModel
+
+class LoanerViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
