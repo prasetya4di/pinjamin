311767a466fa8e19fcf8235c0b7b3c3d04f0c472
praset <prasetyaarfanto@gmail.com>
2022-07-17 22:05:02 +0700

    add create and edit category view model

---
+++ app/src/main/java/com/project/pinjamin/ui/category/create/CreateCategoryViewModel.kt
@@ -0,0 +1,6 @@
+package com.project.pinjamin.ui.category.create
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class CreateCategoryViewModel @Inject constructor() : ViewModel()
---
+++ app/src/main/java/com/project/pinjamin/ui/category/update/UpdateCategoryViewModel.kt
@@ -0,0 +1,6 @@
+package com.project.pinjamin.ui.category.update
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class UpdateCategoryViewModel @Inject constructor() : ViewModel()
